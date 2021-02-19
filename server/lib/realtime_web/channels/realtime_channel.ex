08e70dacb67b7b56647451c6fcb0008cefc2edda
Wen Bo Xie <wenbo.xie3@gmail.com>
2021-02-19 01:19:44 -0500

    feat: remove all (*) event broadcasting

--- server/lib/realtime_web/channels/realtime_channel.ex
+++ server/lib/realtime_web/channels/realtime_channel.ex
@@ -9,6 +9,7 @@ defmodule RealtimeWeb.RealtimeChannel do
   @doc """
   Disabling inward messages from the websocket.
   """
+
   # def handle_in(event_type, payload, socket) do
   #   Logger.info event_type
   #   broadcast!(socket, event_type, payload)
@@ -19,7 +20,6 @@ defmodule RealtimeWeb.RealtimeChannel do
   Handles a full, decoded transation.
   """
   def handle_realtime_transaction(topic, txn) do
-    RealtimeWeb.Endpoint.broadcast_from!(self(), topic, "*", txn)
     RealtimeWeb.Endpoint.broadcast_from!(self(), topic, txn.type, txn)
   end
 end
